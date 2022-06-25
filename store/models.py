5f2372641c69da24cf87fecf0c1d793437afa9a9
kaixuan <kaixuan@acepointer.sg>
2022-06-25 14:23:54 +0800

    address model added

--- store/models.py
+++ store/models.py
@@ -41,3 +41,8 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_PENDING
         )
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer= models.OneToOneField(Customer, on_delete=models.CASCADE, priamry_key=True)
